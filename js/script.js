1edba70d0d83c5c5f4dcfa42a30b0e889507fcf5
Krisvinil <46937474+Krisvinil@users.noreply.github.com>
2019-03-18 22:42:20 +0300

    добавлен раздел js

---
+++ js/script.js
@@ -0,0 +1,58 @@
+var link = document.querySelector(".button-write-to-us");
+var popup = document.querySelector(".pop-up");
+var close = popup.querySelector(".modal-close")
+var form = popup.querySelector(".form")
+var userName = popup.querySelector("[name=name]");
+var email = popup.querySelector("[name=email]");
+
+var isStorageSupport = true;
+var storage = "";
+
+try {
+  storage = localStorage.getItem("name");
+} catch (err) {
+  isStorageSupport = false;
+}
+
+link.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  popup.classList.add("modal-show")
+  if (storage) {
+    userName.value = storage;
+    email.focus();
+  } else {
+    userName.focus();
+  }
+});
+
+close.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  popup.classList.remove("modal-show");
+  popup.classList.remove("modal-error");
+  if (storage) {
+    userName.value = storage;
+  }
+});
+
+form.addEventListener("submit", function (evt) {
+  if (!userName.value || !email.value) {
+    evt.preventDefault();
+    popup.classList.remove("modal-error");
+    popup.offsetWidth = popup.offsetWidth;
+    popup.classList.add("modal-error");
+  } else {
+    if (isStorageSupport) {
+      localStorage.setItem("name", userName.value);
+    }
+  }
+});
+
+window.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
+    evt.preventDefault();
+    if (popup.classList.contains("modal-show")) {
+      popup.classList.remove("modal-show");
+      popup.classList.remove("modal-error");
+    }
+  }
+});
